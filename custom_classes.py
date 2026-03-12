68ac093ee2286c66e68bc88fb16360d58ca0c88c
Mimi iInda <shsahd@gmail.com>
2026-03-12 22:19:35 +0300

    div method

--- custom_classes.py
+++ custom_classes.py
@@ -4,3 +4,10 @@ class Calculator:
 
     def get_current_val(self):
         return self.__current_val
+    
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+
+        self.__current_val = x / y
+        return self.__current_val 
