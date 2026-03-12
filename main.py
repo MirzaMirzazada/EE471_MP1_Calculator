6e9edb9ec55b86985a59a38f6544894a07a11835
Mimi iInda <shsahd@gmail.com>
2026-03-12 23:08:50 +0300

    final version

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from custom_classes import Calculator
 calc = Calculator()
 
 print(calc.get_current_val())
+#print((10 + 5) * 2 / 3)
 
-print(( 10 + 5 )* 2 / 3 ) 
+result = calc.add(10, 5)
+result = calc.multiply(result, 2)
+result = calc.divide(result, 3)
+print(result)
+
