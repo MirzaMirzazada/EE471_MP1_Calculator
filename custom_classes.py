06a3887190d30410fd2ed5ee8502205f9bd92932
Mimi iInda <shsahd@gmail.com>
2026-03-12 22:50:16 +0300

    final version

--- custom_classes.py
+++ custom_classes.py
@@ -3,36 +3,22 @@ class Calculator:
         self.__current_val = 0
 
     def get_current_val(self):
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         return self.__current_val
     
     def add(self, x, y):
         self.__current_val = x + y
->>>>>>> feat/add
-=======
         return self.__current_val
-
+    
     def subtract(self, x, y):
         self.__current_val = x - y
->>>>>>> feat/sub
         return self.__current_val
-=======
+    
+    def multiply(self, x, y):
+        self.__current_val = x * y
         return self.__current_val
     
     def divide(self, x, y):
         if y == 0:
             raise ValueError("Cannot divide by zero")
-
         self.__current_val = x / y
-        return self.__current_val 
->>>>>>> feat/div
-=======
         return self.__current_val
-    def multiply(self, x, y):
-        self.__current_val = x * y
-        return self.__current_val
->>>>>>> feat/mul
---
+++ main.py
@@ -0,0 +1,7 @@
+from custom_classes import Calculator
+
+calc = Calculator()
+
+print(calc.get_current_val())
+
+print(( 10 + 5 )* 2 / 3 ) 
