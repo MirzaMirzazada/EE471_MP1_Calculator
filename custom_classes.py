731f8bf48164bed24c1e9f4d7ed0096798f9cc66
Mimi iInda <shsahd@gmail.com>
2026-03-12 22:11:31 +0300

    Correct mistake

--- custom_classes.py
+++ custom_classes.py
@@ -1,6 +1,6 @@
 class Calculator:
     def __init__(self):
-        self.current_val = 0
+        self.__current_val = 0
 
     def get_current_val(self):
-        return self.current_val
+        return self.__current_val
